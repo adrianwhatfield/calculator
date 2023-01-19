7e4c3f7784f0ce29ec03e348b14808f4fb132133
Adrian Hatfield <adrianhatfield23@gmail.com>
2023-01-19 13:59:04 +0000

    Create operate() function

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
+
+let operator = "add";
+
+function operate(a, b) {
+    if (operator == "add") {
+        return add(a, b);
+    } else if (operator == "subtract") {
+        return subtract(a, b);
+    } else if (operator == "multiply") {
+        return multiply(a, b);
+    } else if (operator == "divide") {
+        return divide(a, b);
+    };
+};
